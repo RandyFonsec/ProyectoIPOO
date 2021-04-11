324651d1539ab828ba7b9d3a4fe1445cf54d82b1
RandyFonsec <49202815+RandyFonsec@users.noreply.github.com>
2021-04-11 17:52:04 -0600

    Update Casillero.java

--- ProyectoIPOO/src/main/java/Modelo/Casillero.java
+++ ProyectoIPOO/src/main/java/Modelo/Casillero.java
@@ -5,10 +5,45 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Randy
  */
 public class Casillero {
-    int a;
+    private int numero;
+    private boolean libre;
+    private ArrayList<Entregable> articulos = new ArrayList();
+
+    public Casillero(int numero, boolean libre) {
+        this.numero = numero;
+        this.libre = libre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public boolean isLibre() {
+        return libre;
+    }
+
+    public void setLibre(boolean libre) {
+        this.libre = libre;
+    }
+
+    public ArrayList<Entregable> getArticulos() {
+        return articulos;
+    }
+
+    public void setArticulos(ArrayList<Entregable> articulos) {
+        this.articulos = articulos;
+    }
+    
+    
 }
