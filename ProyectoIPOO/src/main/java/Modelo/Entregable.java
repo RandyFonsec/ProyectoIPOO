402b5fb8f4204bc428320ed9713f58832db9d5ed
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-04-11 18:20:29 -0600

    Update Entregable.java

--- ProyectoIPOO/src/main/java/Modelo/Entregable.java
+++ ProyectoIPOO/src/main/java/Modelo/Entregable.java
@@ -22,5 +22,74 @@ public class Entregable {
     private double pesoKilogramos;
     private TipoEntregable tipo;
     private Calendar fechaRetiro;
+
+    public int getNumeroReferencia() {
+        return numeroReferencia;
+    }
+
+    public void setNumeroReferencia(int numeroReferencia) {
+        this.numeroReferencia = numeroReferencia;
+    }
+
+    public boolean isEntregado() {
+        return entregado;
+    }
+
+    public void setEntregado(boolean entregado) {
+        this.entregado = entregado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
+
+    public Cliente getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(Cliente destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public double getPesoKilogramos() {
+        return pesoKilogramos;
+    }
+
+    public void setPesoKilogramos(double pesoKilogramos) {
+        this.pesoKilogramos = pesoKilogramos;
+    }
+
+    public TipoEntregable getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEntregable tipo) {
+        this.tipo = tipo;
+    }
+
+    public Calendar getFechaRetiro() {
+        return fechaRetiro;
+    }
+
+    public void setFechaRetiro(Calendar fechaRetiro) {
+        this.fechaRetiro = fechaRetiro;
+    }
+
+    @Override
+    public String toString() {
+        return "Entregable{" + "numeroReferencia=" + numeroReferencia + ", entregado=" + entregado + ", descripcion=" + descripcion + ", remitente=" + remitente + ", destinatario=" + destinatario + ", pesoKilogramos=" + pesoKilogramos + ", tipo=" + tipo + ", fechaRetiro=" + fechaRetiro + '}';
+    }
     
 }
