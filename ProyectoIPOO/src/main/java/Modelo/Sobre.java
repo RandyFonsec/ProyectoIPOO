476cab53321aaca6dd61d573cb75fced129138a5
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-04-11 18:26:49 -0600

    Update Sobre.java

--- ProyectoIPOO/src/main/java/Modelo/Sobre.java
+++ ProyectoIPOO/src/main/java/Modelo/Sobre.java
@@ -9,6 +9,30 @@ package Modelo;
  *
  * @author Randy
  */
-public class Sobre {
+public class Sobre extends Entregable {
     
+     private TipoSobre tipoSobre;
+     private String contenido;
+
+    public TipoSobre getTipoSobre() {
+        return tipoSobre;
+    }
+
+    public void setTipoSobre(TipoSobre tipoSobre) {
+        this.tipoSobre = tipoSobre;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    @Override
+    public String toString() {
+        return "Sobre{" + "tipoSobre=" + tipoSobre + ", contenido=" + contenido + '}';
+    }
+
 }
