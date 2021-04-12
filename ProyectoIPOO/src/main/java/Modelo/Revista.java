9aab44910dc0a888889855782a6d68768ee45c67
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-04-11 18:27:54 -0600

    Update Revista.java

--- ProyectoIPOO/src/main/java/Modelo/Revista.java
+++ ProyectoIPOO/src/main/java/Modelo/Revista.java
@@ -9,6 +9,39 @@ package Modelo;
  *
  * @author Randy
  */
-public class Revista {
+public class Revista extends Entregable {
+    
+    private String nombre;
+    private boolean catalogo;
+    private TemaRevista tema;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(boolean catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public TemaRevista getTema() {
+        return tema;
+    }
+
+    public void setTema(TemaRevista tema) {
+        this.tema = tema;
+    }
+
+    @Override
+    public String toString() {
+        return "Revista{" + "nombre=" + nombre + ", catalogo=" + catalogo + ", tema=" + tema + '}';
+    }
     
 }
